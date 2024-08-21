0487bdfafe423cf7112cb1a6c17f7b3f58d2970e
s8140156 <s8140156@gmail.com>
2024-08-21 17:37:57 +0800

    #34練習_20240821

--- index.php
+++ index.php
@@ -1,27 +1,37 @@
 <?php
 
-// #34 Sum of the first nth term of Series
+// #35 Sum of odd numbers
 
-// Your task is to write a function which returns the n-th term of the following series, which is the sum of the first n terms of the sequence (n is the input parameter).
-// series: 1+1/4+1/7+1/10+1/13...You will need to figure out the rule of the series to complete this.
+// Given the triangle of consecutive odd numbers:
+// 1
+// 3     5
+// 7     9    11
+// 13    15    17    19
+// 21    23    25    27    29
+// ...
 
-// Rules
-// You need to round the answer to 2 decimal places and return it as String.
-// If the given value is 0 then it should return "0.00".
-// You will only be given Natural Numbers as arguments.
+// Calculate the sum of the numbers in the nth row of this triangle (starting at index 1) e.g.: (Input --> Output)
 
-function series_sum($n) {
-    // Your code here
+function rowSumOddNumbers($n) {
+    // your code here
+    $oddNum=$n*($n-1)+1;
     $sum=0;
     for($i=0;$i<$n;$i++){
-        $sum+=1/(1+3*$i);
+        $sum+=$oddNum+($i*2); //注意是$i*2 因為都是從該迴圈去累加
     }
-    return number_format($sum,2);
+    return $sum;
   }
 
-  //point:先注意1/幾的規則 下一個都是n+3
-  // number_format():數值格式化為指定的小數位數，並將結果轉換為字串。這個函式特別適合用來處理需要顯示固定小數位數的數值，並且它會自動進行四捨五入。
+  // “觀察”奇數金字塔 會發現每行$n的三次方就是該行的奇數總和
+//   function rowSumOddNumbers($n) {
+//     // your code here
+//     return $n**3;
+//     or return $n*$n*$n;
+//   }
 
-  echo series_sum(3);
+  echo rowSumOddNumbers(3);
+
+
+
 
 ?>
