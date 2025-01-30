32cc10fd3667baa21ce5edacf64d3ea1be439985
s8140156 <s8140156@gmail.com>
2025-01-31 01:43:29 +0800

    no_78_20250131

---
+++ no_77.php
@@ -0,0 +1,20 @@
+<?php
+
+// #77 Reversed Strings
+// Complete the solution so that it reverses the string passed into it.
+// 'world'  =>  'dlrow'
+  
+function solution(string $str): string {
+  return strrev($str); // Your code here
+}
+
+
+echo solution('sort');
+
+// strrev(): 反轉字符 麻煩請你記起來= =
+// strrev(string $string):string
+// 參數：string
+// return: string
+
+
+?>
