1940f008526531d73c26e0207047f83f00c04333
s8140156 <s8140156@gmail.com>
2025-07-29 21:08:30 +0800

    no_86_20250729

--- index.php
+++ index.php
@@ -1,37 +1,26 @@
 <?php
 
-// #85 What is between
+// #86 Removing Elements
 
-// Complete the function that takes two integers (a, b, where a < b) and return an array of all integers between the input parameters, including them.
-// a=1 b=4 -->[1,2,3,4]
+// Take an array and remove every second element from the array. Always keep the first element and start removing with the next element
+// Example:["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
 
-function between(int $a, int $b): array {
-    $array=[];
-    for($i=$a; $i<=$b; $i++){
-        $array[]=$i;
+function removeEveryOther($array) {
+  $result=[];
+  foreach($array as $idx => $value){
+    if($idx % 2 == 0){
+        $result[$idx]=$value;
     }
-  return $array;
+  }
+  return $result;
 }
+// 這個好玩的是 雖然題目是不要偶數的元素 但實際是用選index %2 餘數0 做保留(ex.index 0,2,4...保留)
+// 注意是用index篩選 但是把"值 value"放進陣列裡
+// 原先寫$result[]=$value; => 會將index重新編排 ex.[0 => 1, 1 => 5, 2 => 9]
+// $result[$idx]=$value; => 保留原index ex.[0 => 1, 2 => 5, 4 =>9]
 
-// 簡潔語法
-// function between(int $a, int $b): array {
-//     return range($a, $b);
-// }
+print_r(removeEveryOther([1,3,5,7,9,11]));
 
-// range()
-// 語法：range(start(起始), end(結束), step = 1(間距 預設1))
-// 也可以用在字元喔 print_r(range('a', 'e'));
-
-
-
-// 面面俱到法
-// function between(int $a, int $b): array {
-//     return $a < $b ? range($a, $b) : range($b, $a);
-// }
-
-
-
-print_r(between(-3, 7));
 
 
 ?>
---
+++ no_85.php
@@ -0,0 +1,37 @@
+<?php
+
+// #85 What is between
+
+// Complete the function that takes two integers (a, b, where a < b) and return an array of all integers between the input parameters, including them.
+// a=1 b=4 -->[1,2,3,4]
+
+function between(int $a, int $b): array {
+    $array=[];
+    for($i=$a; $i<=$b; $i++){
+        $array[]=$i;
+    }
+  return $array;
+}
+
+// 簡潔語法
+// function between(int $a, int $b): array {
+//     return range($a, $b);
+// }
+
+// range()
+// 語法：range(start(起始), end(結束), step = 1(間距 預設1))
+// 也可以用在字元喔 print_r(range('a', 'e'));
+
+
+
+// 面面俱到法
+// function between(int $a, int $b): array {
+//     return $a < $b ? range($a, $b) : range($b, $a);
+// }
+
+
+
+print_r(between(-3, 7));
+
+
+?>
