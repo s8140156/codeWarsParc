3f7e25d545665d7c0da0e4ece6e542e503c82cff
s8140156 <s8140156@gmail.com>
2025-08-04 14:28:58 +0800

    no_87_20250804

--- index.php
+++ index.php
@@ -1,38 +1,37 @@
 <?php
 
-// #86 Removing Elements
+// # 87 Sort Numbers
 
-// Take an array and remove every second element from the array. Always keep the first element and start removing with the next element
-// Example:["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
+// Finish the solution so that it sorts the passed in array of numbers. If the function passes in an empty array or null/nil value then it should return an empty array.
 
-function removeEveryOther($array) {
-  $result=[];
-  foreach($array as $idx => $value){
-    if($idx % 2 == 0){
-        $result[$idx]=$value;
+
+function solution($nums) {
+    if(!empty($nums)){
+        sort($nums); // sort() 就地排序（in-place sort）函數，會直接修改 $nums 這個陣列的內容; 但他回傳是bool (true/false) 不是排序後的陣列本身
+        // sort(array &$array, int $flags = SORT_REGULAR): true
+        return $nums; // 所以要回傳排序過後的$nums
+    }else{
+        return [];
     }
-  }
-  return $result;
 }
-// 這個好玩的是 雖然題目是不要偶數的元素 但實際是用選index %2 餘數0 做保留(ex.index 0,2,4...保留)
-// 注意是用index篩選 但是把"值 value"放進陣列裡
-// 原先寫$result[]=$value; => 會將index重新編排 ex.[0 => 1, 1 => 5, 2 => 9]
-// $result[$idx]=$value; => 保留原index ex.[0 => 1, 2 => 5, 4 =>9]
 
-// 另一種思考(以除掉奇數為主)
-// function removeEveryOther($array) {
-//   $result=[];
-//   foreach($array as $idx => $value){
-//     if($idx % 2){
-//         unset($array[$idx]);
-//     }
-//   }
-//   return $result;
+
+// 簡潔寫法
+// function solution($nums) : array {
+//   $nums = $nums ?? [];
+//   sort($nums);
+//   return $nums;
 // }
-// 保留索引為偶數的項目，移除索引為奇數的項目
-// unset()保留原始索引不變
 
-print_r(removeEveryOther([1,3,5,7,9,11]));
+// 如果 $nums 沒有被設定（例如是 null 或根本不存在），就給它預設為空陣列 [], 否則保留原本的值
+// 使用了 PHP 的「null 合併運算子（null coalescing operator）??」
+// 等價
+// if (!isset($nums)) {
+//     $nums = [];
+// }
+
+
+print_r(solution([]));
 
 
 
---
+++ no_86.php
@@ -0,0 +1,39 @@
+<?php
+
+// #86 Removing Elements
+
+// Take an array and remove every second element from the array. Always keep the first element and start removing with the next element
+// Example:["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
+
+function removeEveryOther($array) {
+  $result=[];
+  foreach($array as $idx => $value){
+    if($idx % 2 == 0){
+        $result[$idx]=$value;
+    }
+  }
+  return $result;
+}
+// 這個好玩的是 雖然題目是不要偶數的元素 但實際是用選index %2 餘數0 做保留(ex.index 0,2,4...保留)
+// 注意是用index篩選 但是把"值 value"放進陣列裡
+// 原先寫$result[]=$value; => 會將index重新編排 ex.[0 => 1, 1 => 5, 2 => 9]
+// $result[$idx]=$value; => 保留原index ex.[0 => 1, 2 => 5, 4 =>9]
+
+// 另一種思考(以除掉奇數為主)
+// function removeEveryOther($array) {
+//   $result=[];
+//   foreach($array as $idx => $value){
+//     if($idx % 2){
+//         unset($array[$idx]);
+//     }
+//   }
+//   return $result;
+// }
+// 保留索引為偶數的項目，移除索引為奇數的項目
+// unset()保留原始索引不變
+
+print_r(removeEveryOther([1,3,5,7,9,11]));
+
+
+
+?>
